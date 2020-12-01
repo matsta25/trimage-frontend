c5d69c36a58e09a913f3669c800ea054bc832e5a
matsta25 <matim2112@gmail.com>
2020-12-01 16:20:10 +0100

    feat(app): add working flow v22

--- src/app/features/trimage/config/my-rx-stomp.config.ts
+++ src/app/features/trimage/config/my-rx-stomp.config.ts
@@ -2,5 +2,6 @@ import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
 
 export const myRxStompConfig: InjectableRxStompConfig = {
   // brokerURL: 'ws://localhost:8080/trimage',
-  brokerURL: 'ws://trimage-backend.herokuapp.com/trimage',
+  // brokerURL: 'ws://trimage-backend.herokuapp.com/trimage',
+  brokerURL: 'wss://trimage-backend.herokuapp.com/trimage',
 };
