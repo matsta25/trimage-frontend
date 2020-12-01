9961ddc7e1dcd8fed074aad61c51a2d0d8951b4d
matsta25 <matim2112@gmail.com>
2020-12-01 23:02:22 +0100

    feat(app): add working version v1 - PROD

--- src/app/features/trimage/config/my-rx-stomp.config.ts
+++ src/app/features/trimage/config/my-rx-stomp.config.ts
@@ -1,7 +1,6 @@
 import { InjectableRxStompConfig } from '@stomp/ng2-stompjs';
 
 export const myRxStompConfig: InjectableRxStompConfig = {
-  brokerURL: 'ws://localhost:8080/trimage',
-  // brokerURL: 'ws://trimage-backend.herokuapp.com/trimage',
-  // brokerURL: 'wss://trimage-backend.herokuapp.com/trimage',
+  // brokerURL: 'ws://localhost:8080/trimage',
+  brokerURL: 'wss://trimage-backend.herokuapp.com/trimage',
 };
